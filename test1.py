e97f88b811d49b90e719740553ec8aeff8926247
RinaChiang <41978200+RinaChiang@users.noreply.github.com>
2019-03-06 15:26:30 +0800

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,9 @@
+j=1
+for item in ['apple','pear','apricot','cherry','peach']:
+    print(j,item)
+    j+=1 
+
+j=0
+for item in ['apple','pear','apricot','cherry','peach','banana']:
+    print(j,item)
+    j+=1 
